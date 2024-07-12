77f3184673331c7e318b3bce69765a06877929a3
Nicola Chimenti <nicola.chimenti.work@gmail.com>
2024-07-12 17:31:34 +0200

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -10,8 +10,8 @@ import pandas as pd
 # Introduction for the user
 st.write("# LET'S ANALYZE THE SEASONALITY OF AN ASSET")
 st.write("### You have just to set: when to start with the monitoration,when to end and which is the asset to see")
-st.write("### Please, note that it has been used the YAHOO! FINANCE API, so you have to select the ticker of the asset based on the yahoo!finance database")
-#st.markdown("### You can check the name of the asset you're searching right [here](%s)", % https://finance.yahoo.com/lookup/)
+st.write("Please, note that it has been used the YAHOO! FINANCE API, so you have to select the ticker of the asset based on the yahoo!finance database")
+#st.markdown("You can check the name of the asset you're searching right [here](%s)", % https://finance.yahoo.com/lookup/)
 
 AnnoPartenz = st.number_input("Starting year: ",min_value = 1850, step = 1)
 
